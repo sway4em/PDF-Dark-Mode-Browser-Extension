19330478f8e5011738e207ba668704dd8d41de31
Swayam Chidrawar <schidraw@calpoly.edu>
2023-07-08 21:20:16 -0700

    Popup box color changes based on  current mode

--- popup.js
+++ popup.js
@@ -1,3 +1,10 @@
 document.getElementById('toggle-dark-mode').addEventListener('change', () => {
     chrome.runtime.sendMessage({ action: 'runScript' });
+    if (event.target.checked) {
+        document.body.style.backgroundColor = '#333';
+    } else {
+        document.body.style.backgroundColor = 'white';
+    }
 });
+
+
