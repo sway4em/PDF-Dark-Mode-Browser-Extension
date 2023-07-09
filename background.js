73e24f70e6e4b0e420d424e698ef0dda4cd22241
Swayam Chidrawar <schidraw@calpoly.edu>
2023-07-08 21:04:27 -0700

    Add background.js

---
+++ background.js
@@ -0,0 +1,29 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'runScript') {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                function: runScript
+            });
+        });
+    }
+});
+
+function runScript() {
+    const overlay = document.createElement("div");
+
+    const css = `
+        position: fixed;
+        pointer-events: none;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: white;
+        mix-blend-mode: difference;
+        z-index: 1;
+    `
+    overlay.setAttribute("style", css);
+
+    document.body.appendChild(overlay);
+}
