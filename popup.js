f5e1cd294d08ac110980110845eacf1f9777ae94
Swayam Chidrawar <schidraw@calpoly.edu>
2023-07-08 21:23:59 -0700

    Icons next to slider change based on the current mode

--- popup.js
+++ popup.js
@@ -1,10 +1,12 @@
-document.getElementById('toggle-dark-mode').addEventListener('change', () => {
+document.getElementById('toggle-dark-mode').addEventListener('change', (event) => {
     chrome.runtime.sendMessage({ action: 'runScript' });
     if (event.target.checked) {
         document.body.style.backgroundColor = '#333';
+        document.querySelector('.switch-container img:nth-child(1)').src = 'https://img.icons8.com/ios/50/ffffff/sun--v1.png';
+        document.querySelector('.switch-container img:nth-child(3)').src = 'https://img.icons8.com/ios-glyphs/30/ffffff/bright-moon.png';
     } else {
         document.body.style.backgroundColor = 'white';
+        document.querySelector('.switch-container img:nth-child(1)').src = 'https://img.icons8.com/ios/50/000000/sun--v1.png';
+        document.querySelector('.switch-container img:nth-child(3)').src = 'https://img.icons8.com/ios-glyphs/30/000000/bright-moon.png';
     }
 });
-
-
